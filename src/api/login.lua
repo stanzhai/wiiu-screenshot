e5cb835a4d7d3b8ee44bd58c4ca9e3f68dab89ce
Stan Zhai <mail@stanzhai.site>
2017-06-06 01:01:35 +0800

    update login method

--- src/api/login.lua
+++ src/api/login.lua
@@ -2,10 +2,11 @@ local config = require("config")
 ngx.req.read_body()
 local args, err = ngx.req.get_post_args()
 if not args or args.password ~= config.password then
-    ngx.say("failed to login")
+    -- do nothing
 else
     local session = require "resty.session".start()
     session.data.logged = true
     session:save()
-    ngx.say("ok")
 end
+
+ngx.redirect("/")
